2753ae32a1a056272ca1ac538d4822e62de1e14f
Ryan J. Geyer <me@ryangeyer.com>
2016-12-28 12:24:58 -0800

    Use docker pipeline plugin directly rather than trying to use the DSL

--- src/devops/docker.groovy
+++ src/devops/docker.groovy
@@ -1,9 +1,11 @@
 package devops.docker
 
+def dockerPlugin = new org.jenkinsci.plugins.docker.workflow.Docker()
+
 def buildImage(directory, imageName) {
-  docker.withServer('tcp://localhost:2375') {
+  dockerPlugin.withServer('tcp://localhost:2375') {
     dir(directory) {
-      docker.build imageName
+      dockerPlugin.build imageName
     }
   }
 }
