297aa85b0966351404045b8fe5cdfe02ea791aa9
Ryan J. Geyer <me@ryangeyer.com>
2016-12-28 12:26:00 -0800

    Scope is important

--- src/devops/docker.groovy
+++ src/devops/docker.groovy
@@ -1,8 +1,7 @@
 package devops.docker
 
-def dockerPlugin = new org.jenkinsci.plugins.docker.workflow.Docker()
-
 def buildImage(directory, imageName) {
+  def dockerPlugin = new org.jenkinsci.plugins.docker.workflow.Docker()
   dockerPlugin.withServer('tcp://localhost:2375') {
     dir(directory) {
       dockerPlugin.build imageName
